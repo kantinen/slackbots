4d1c6a8e258205e06174674ef0f73acb87b00fc2
Alexander M Scheurer <alexandermscheurer@gmail.com>
2017-11-07 01:41:40 +0100

    Make working edition of Slackbot and update README
    README now reflects project. Still needs to implement glob patterns

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
+# Installation
+First clone the repo and run the following commands:
+```bash
+# The Slack API
+$ go get -u github.com/nlopes/slack
 
-The api we are using:
+# The YAML package
+$ go get -u gopkg.in/yaml.v2
+```
 
-https://github.com/nlopes/slack
+Now you can build the bot by running the command: `$ go build`\
+This will create a file called `slackbots` which can be run from the command line. Be sure
+to check out the [Environment variables](#environment-variables) section.
+
+# Usage
+First add the bot to your workspace, afterwards invite the bot to the relevant channels.
+The bot reacts to three commands:
+```
+!create product [name]
+!set cost [cost] [name]
+!get prices [name]
+```
+
+The first command creates a product with the given name. The name may contain spaces.\
+The second command sets the price of a given product to the cost in ører.\
+The third command makes the bot send a message to the channel with the prices for the
+product.
 
 # Environment variables
 - `KANTINE_DB` - must be set for bot to work
 - `SLACK_API_TOKEN` - must be set for Slack to work
+- `GO_ENV` - should be set to 'production' in production mode to disable logging
+
+# APIs
+The api we are using:
+
+https://github.com/nlopes/slack
+https://github.com/go-yaml/yaml/tree/v2
--- slackbot.go
+++ slackbot.go
@@ -3,17 +3,23 @@ package main
 import (
     "fmt"
     "log"
+    "strings"
     "os"
     "os/exec"
-
+    "strconv"
     "github.com/nlopes/slack"
 )
 
 func main() {
+    pullDB()
+
     api := slack.New(os.Getenv("SLACK_API_TOKEN"))
-    logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
-    slack.SetLogger(logger)
-    api.SetDebug(true)
+
+    if strings.ToLower(os.Getenv("GO_ENV")) != "production" {
+        logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
+        slack.SetLogger(logger)
+        api.SetDebug(true)
+    }
 
     rtm := api.NewRTM()
     go rtm.ManageConnection()
@@ -27,14 +33,54 @@ func main() {
         case *slack.ConnectedEvent:
             fmt.Println("Infos:", ev.Info)
             fmt.Println("Connection counter:", ev.ConnectionCount)
-            // Replace #general with your Channel ID
-            rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "gogadget-test"))
 
         case *slack.MessageEvent:
-            if len(ev.Text) >= 9 && ev.Text[0:9] == "!set cost" {
-                    handleSet(ev.Text[10:])
-            } else if len(ev.Text) >= 11 && ev.Text[0:11] == "!get prices" {
-                    handleGet(ev.Text[11:])
+            if len(ev.Text) > 9 && ev.Text[0:9] == "!set cost" {
+                err := handleSet(ev, api)
+                if err != nil {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                        fmt.Sprintf("'%s'\n%v\nUsage: !set cost [cost] [name]",
+                            ev.Text[10:],
+                            err),
+                        ev.Msg.Channel))
+                } else {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                        fmt.Sprintf("'%s'\nProduct price sucessfully set", ev.Text[10:]),
+                        ev.Msg.Channel))
+                }
+            } else if len(ev.Text) > 11 && ev.Text[0:11] == "!get prices" {
+                costs, err := handleGet(ev, api)
+                if err != nil {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                        fmt.Sprintf("'%s'\n%v\nUsage: !get prices [name]",
+                            ev.Text[12:],
+                            err),
+                        ev.Msg.Channel))
+                } else {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                        fmt.Sprintf("'%s'\n%+v\n", ev.Text[12:], costs),
+                        ev.Msg.Channel))
+                }
+            } else if len(ev.Text) > 15 && ev.Text[0:15] == "!create product" {
+                err := handleCreate(ev, api)
+                if err != nil {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                            fmt.Sprintf("'%s'\n%v\nUsage: !create product [cost] [name]",
+                                ev.Text[16:],
+                                err),
+                            ev.Msg.Channel))
+                } else {
+                    rtm.SendMessage(rtm.NewOutgoingMessage(
+                            fmt.Sprintf("'%s'\nCreated the new product", ev.Text[16:]),
+                            ev.Msg.Channel))
+                }
+            } else if len(ev.Text) == 5 && ev.Text == "!help" {
+                rtm.SendMessage(rtm.NewOutgoingMessage(
+                            "Usage: !create product [cost] [name]\n" +
+                            "Usage: !set cost [cost] [name]\n" +
+                            "Usage: !get prices [name]\n" +
+                            "Usage: !help - display this help and exit",
+                            ev.Msg.Channel))
             }
 
         case *slack.InvalidAuthEvent:
@@ -47,17 +93,98 @@ func main() {
             // fmt.Printf("Unexpected: %v\n", msg.Data)
         }
     }
+}
+
+func handleSet(ev *slack.MessageEvent, api *slack.Client) error{
+    err := pullDB()
+    if err != nil {
+        return err
+    }
+
+    text := ev.Text[10:]
+
+    commands := strings.Split(text, " ")
+
+    cost, err := strconv.Atoi(commands[0])
+    if err != nil {
+        return err
+    }
+
+    name := strings.Join(commands[1:], " ")
+
+    err = setProductPrice(name, cost)
+    if err != nil {
+        return err
+    }
+
+    err = updateDB(ev, api)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func handleGet(ev *slack.MessageEvent, api *slack.Client) (Costs, error){
+    err := pullDB()
+    if err != nil {
+        return Costs{}, err
+    }
+
+    name := ev.Text[12:]
+
+    costs, err := getProductPrice(name)
+    if err != nil {
+        return Costs{}, err
+    }
+
+    err = updateDB(ev, api)
+    if err != nil {
+        return Costs{}, err
+    }
+    return costs, nil
+}
+
+func handleCreate(ev *slack.MessageEvent, api *slack.Client) error {
+    err := pullDB()
+    if err != nil {
+        return err
+    }
+
+    text := ev.Text[16:]
+
+    commands := strings.Split(text, " ")
+
+    cost, err := strconv.Atoi(commands[0])
+    if err != nil {
+        return err
+    }
+
+    name := strings.Join(commands[1:], " ")
+
+    err = createProduct(name, cost)
+    if err != nil {
+        return err
+    }
+
+    err = updateDB(ev, api)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func updateDB(ev *slack.MessageEvent, api *slack.Client) error{
     // To run the update script
-    cmd := exec.Command("./update.sh", "username", "command")
-    cmd.Run()
+    cmd := exec.Command("./updateDB.sh", getUsername(ev, api), ev.Text)
+    return cmd.Run()
 }
 
-func handleSet(a string) {
-    fmt.Printf("Ran handleSet %s\n\n", a)
-    return
+func pullDB() error{
+    cmd := exec.Command("./pullDB.sh")
+    return cmd.Run()
 }
 
-func handleGet(a string) {
-    fmt.Printf("Ran handleGet %s\n\n", a)
-    return
+func getUsername(ev *slack.MessageEvent, api *slack.Client) string{
+    user, _ := api.GetUserInfo(ev.Msg.User)
+    return user.RealName
 }
